5a1a768f732d4b6bf4bdf3472d62ff49ddad2ce9
coseo12 <momo_12@naver.com>
2020-09-18 09:42:02 +0900

    add

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -14,46 +14,46 @@ import { ref, inject } from 'vue';
 import { TodoSymbol } from '../providers/TodoProvider';
 
 export default {
-  name: 'Input',
-  setup() {
-    const todoItem = ref(null);
-    const { addTodo } = inject(TodoSymbol);
+	name: 'Input',
+	setup() {
+		const todoItem = ref(null);
+		const { addTodo } = inject(TodoSymbol);
 
-    const addTodoList = () => {
-      const item = { id: new Date().getTime(), title: todoItem.value.value };
-      todoItem.value.value = '';
-      addTodo.value(item);
-    };
+		const addTodoList = () => {
+			const item = { id: new Date().getTime(), title: todoItem.value.value };
+			todoItem.value.value = '';
+			addTodo.value(item);
+		};
 
-    return {
-      todoItem,
-      addTodoList,
-    };
-  },
+		return {
+			todoItem,
+			addTodoList,
+		};
+	},
 };
 </script>
 
 <style scoped>
 .add-input {
-  outline: none;
-  width: 200px;
-  border: 1px solid #2ecc71;
-  padding: 10px;
-  margin: 10px;
+	outline: none;
+	width: 200px;
+	border: 1px solid #2ecc71;
+	padding: 10px;
+	margin: 10px;
 }
 .add-btn {
-  outline: none;
-  background: #2ecc71;
-  border: 0;
-  color: #fff;
-  font-weight: 600;
-  font-size: 15px;
-  padding: 9px 20px;
-  cursor: pointer;
+	outline: none;
+	background: #2ecc71;
+	border: 0;
+	color: #fff;
+	font-weight: 600;
+	font-size: 15px;
+	padding: 9px 20px;
+	cursor: pointer;
 }
 .input-wrap {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
--- src/providers/TodoProvider.js
+++ src/providers/TodoProvider.js
@@ -2,17 +2,22 @@ import { provide, reactive, toRefs, readonly } from 'vue';
 
 export const TodoSymbol = Symbol('TodoSymbol');
 
+const useTodo = () => {
+  const todo = reactive({
+    todoList: [],
+    addTodo: item => {
+      todo.todoList.push(item);
+    },
+    deleteTodo: id => {
+      todo.todoList = todo.todoList.filter(item => item.id !== id);
+    },
+  });
+  return todo;
+};
+
 export default {
   setup() {
-    const todo = reactive({
-      todoList: [],
-      addTodo: item => {
-        todo.todoList.push(item);
-      },
-      deleteTodo: id => {
-        todo.todoList = todo.todoList.filter(item => item.id !== id);
-      },
-    });
+    const todo = useTodo();
     provide(TodoSymbol, toRefs(readonly(todo)));
   },
   render() {
